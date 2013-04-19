72b8b01cad20f78e6b74002e5f8b40d461853a1e
Christophe Gesch? <christophe@gesche.org>
2013-04-19 09:53:01 +0200

    start devient status
    les atures fichiers inutiles sont supprim?s

---
+++ scripts/logs/status.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+
+CONFIGFILE=/etc/sphinxsearch/sphinx.conf
+
+echo les sources
+grep -e '^source ' $CONFIGFILE | sort
+
+echo les indexs
+
+grep -e '^index ' $CONFIGFILE | sort
+
+echo les répertoires
+
+grep -e 'path ' $CONFIGFILE | sort
+grep -e '.log$' $CONFIGFILE | sort
+
+TODAYINSPHINXLOG=`date '+%b %d'`
+
+
+QUERYLOG=/var/log/sphinxsearch/query.log 
+
+echo requetes par index
+cat $QUERYLOG | cut -d ']' -f 3 | cut -d '[' -f 2- | sort | uniq -c | sort -r
+
+echo requetes par heure
+cat $QUERYLOG  | cut -d ']' -f 1 | cut -d '[' -f 2- | cut -d ' ' -f 5 | cut -d ':' -f 1  | sort | uniq -c | sort -r
+
+
+echo top search today
+grep $TODAYINSPHINXLOG  $QUERYLOG | sed 's/\(.*\)[^\]]\(.*\)$/\2/' | sort | uniq -c | sort -nr | head 
