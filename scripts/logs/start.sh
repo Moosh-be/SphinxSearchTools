5ac59f931dd00399745abc018ee9d2a84bc170bc
Christophe Gesch? <christophe@gesche.org>
2013-04-19 08:06:20 +0200

    changement de startet suppression fichiers inutiles

--- scripts/logs/start.sh
+++ scripts/logs/start.sh
@@ -3,10 +3,18 @@
 
 CONFIGFILE=/etc/sphinxsearch/sphinx.conf
 
-grep -e '^index ' $CONFIGFILE
-grep -e '^source ' $CONFIGFILE
-grep -e '^path ' $CONFIGFILE
+echo les sources
+grep -e '^source ' $CONFIGFILE | sort
 
+echo les indexs
+
+grep -e '^index ' $CONFIGFILE | sort
+
+echo les répertoires
+
+grep -e '^path ' $CONFIGFILE | sort
+
+TODAYINSPHINXLOG=`date '+%b %d'`
 
 QUERYLOG=/var/log/sphinxsearch/query.log 
 
@@ -16,3 +24,6 @@ cat $QUERYLOG | cut -d ']' -f 3 | cut -d '[' -f 2- | sort | uniq -c | sort -r
 echo requetes par heure
 cat $QUERYLOG  | cut -d ']' -f 1 | cut -d '[' -f 2- | cut -d ' ' -f 5 | cut -d ':' -f 1  | sort | uniq -c | sort -r
 
+
+echo top search today
+grep $TODAYINSPHINXLOG  /var/log/sphinxsearch/query.log | sed 's/\(.*\)[^\]]\(.*\)$/\2/' | sort | uniq -c | sort -nr | head 
