f2da9f12933fa71b06667094a0605df2b32bb1c3
Moosh <christophe@gesche.org>
2013-06-29 17:27:29 +0200

    vieux changements

--- scripts/logs/status.sh
+++ scripts/logs/status.sh
@@ -15,17 +15,21 @@ echo les répertoires
 grep -e 'path ' $CONFIGFILE | sort
 grep -e '.log$' $CONFIGFILE | sort
 
+QUERYLOG=` grep -e ' query_log ' $CONFIGFILE | cut -d'=' -f 2-`
+
 TODAYINSPHINXLOG=`date '+%b %d'`
 
+echo etude du log des call
+echo $QUERYLOG
 
-QUERYLOG=/var/log/sphinxsearch/query.log 
+#QUERYLOG=/var/log/sphinxsearch/query.log 
 
 echo requetes par index
-cat $QUERYLOG | cut -d ']' -f 3 | cut -d '[' -f 2- | sort | uniq -c | sort -r
+sudo cat $QUERYLOG | cut -d ']' -f 3 | cut -d '[' -f 2- | sort | uniq -c | sort -r
 
 echo requetes par heure
-cat $QUERYLOG  | cut -d ']' -f 1 | cut -d '[' -f 2- | cut -d ' ' -f 5 | cut -d ':' -f 1  | sort | uniq -c | sort -r
+sudo cat $QUERYLOG  | cut -d ']' -f 1 | cut -d '[' -f 2- | cut -d ' ' -f 5 | cut -d ':' -f 1  | sort | uniq -c | sort -r
 
 
 echo top search today
-grep $TODAYINSPHINXLOG  $QUERYLOG | sed 's/\(.*\)[^\]]\(.*\)$/\2/' | sort | uniq -c | sort -nr | head 
+sudo grep $TODAYINSPHINXLOG  $QUERYLOG | sed 's/\(.*\)[^\]]\(.*\)$/\2/' | sort | uniq -c | sort -nr | head 
